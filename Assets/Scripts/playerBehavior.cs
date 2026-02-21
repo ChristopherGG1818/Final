f59f0e9c4da0f233c5cf5aa5d4cd91462865d01c
Christopher Garcia <Christopherg1818@gmail.com>
2026-02-21 03:53:45 -0600

    walking and jumping adjusted.

--- Assets/Scripts/playerBehavior.cs
+++ Assets/Scripts/playerBehavior.cs
@@ -4,8 +4,16 @@ using UnityEngine.InputSystem;
 public class playerBehavior : MonoBehaviour
 {
     public float speed = 5f;
+
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckDistance= 0.2f;
+    public LayerMask groundLayer;
+
+
     private Rigidbody2D rb;
     private Animator anim; // Reference to the Animator
+    private bool isGrounded;
 
     void Start()
     {
@@ -25,11 +33,21 @@ public class playerBehavior : MonoBehaviour
 
         rb.velocity = new Vector2(moveInput, rb.velocity.y);
 
+        //cech for the ground
+        isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayer);
+
+        //jump fix this part 
+        // if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded){
+        //     rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        // }
+        //     //moveInput = speed;
+
         // Tell Animator whether we are walking
         if (anim != null)
         {
             bool isWalking = Mathf.Abs(moveInput) > 0;
             anim.SetBool("isWalking", isWalking);
+            anim.SetBool("isGrounded", isGrounded);
 
             // Flip sprite depending on direction
             if (moveInput > 0)
